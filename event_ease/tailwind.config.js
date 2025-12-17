b32aad3e963393dbebb6aac1b691aff7fcd267bd
Nishmithavg <vgnishmitha@gmail.com>
2025-12-17 15:20:56 +0530

    Add event_ease as normal folder (remove nested git)

---
+++ event_ease/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: "#f5faff",
+          100: "#e6f0ff",
+          500: "#1e6fff",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
